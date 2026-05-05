d6ee27399604bd06cda1a176e021f86c426820f1
Deilicis <daviszinkers@gmail.com>
2026-05-05 15:50:13 +0300

    salaboju dashboard loģiku. Dashboard palika uz mekletaja skata, kad maina roles.

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,11 +4,11 @@ import SeekerDashboard from './DashboardPartials/SeekerDashboard.vue';
 import MasterDashboard from './DashboardPartials/MasterDashboard.vue';
 import { Head, usePage } from '@inertiajs/vue3';
 import { computed } from 'vue';
+import { useActiveRole } from '@/composables/useActiveRole';
 import type { AuthUser, Service, JobRequest } from '@/types/models';
 
 const props = defineProps<{
     stats: Record<string, number>;
-    roles: string[];
     recentServices: Service[];
     recentJobRequests: JobRequest[];
     recentApplications: any[];
@@ -18,7 +18,7 @@ const props = defineProps<{
 const page = usePage<{ auth: { user: AuthUser } }>();
 const user = computed(() => page.props.auth.user);
 
-const isMaster = computed(() => props.roles?.includes('master') ?? false);
+const { isMasterActive } = useActiveRole();
 </script>
 
 <template>
@@ -35,14 +35,14 @@ const isMaster = computed(() => props.roles?.includes('master') ?? false);
         <div class="py-8">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <MasterDashboard
-                    v-if="isMaster"
+                    v-if="isMasterActive"
                     :stats="props.stats"
                     :user="user"
                     :recentServices="props.recentServices"
                     :recentApplications="props.recentApplications"
                 />
                 <SeekerDashboard
-                    v-else
+                    v-else-if="!isMasterActive"
                     :stats="props.stats"
                     :user="user"
                     :recentJobRequests="props.recentJobRequests"
